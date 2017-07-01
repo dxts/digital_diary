1dfa3d98bebd59ea46c347ecdcada3f0ceab6483
Dixit Sabharwal <dixitsabharwal05@gmail.com>
2017-07-02 01:22:59 +0300

    Create login.h

---
+++ login.h
@@ -0,0 +1,25 @@
+void login()
+{
+	for(int i=0;i<50;i++)
+	{
+		pass[i]=getch();
+		int intpass=int(pass[i]);
+		if(pass[i]==13)
+		{
+			pass[i]='\0';
+			break;
+			}
+		if(!(intpass>96&&intpass<123||intpass>64&&intpass<91||intpass>32&&intpass<43))
+		{
+			clrscr();
+			gotoxy(20,11);
+			cout<<"Use characters A-Z, a-z, ! \" # $ % & ' ( ) *";
+			gotoxy(25,13);
+			cout<<"press any key to continue...";
+			getch();
+			clrscr();
+			goto enter_login;
+			}
+		cout<<'*';
+		}
+	}
