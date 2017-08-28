fd3b2a19a439c001a6531a63f180c882ba25fa27
Dixit Sabharwal <dixitsabharwal05@gmail.com>
2017-08-28 16:54:42 +0300

    added error_mssg()

--- misc.h
+++ misc.h
@@ -1,17 +1,17 @@
 char* getpass()
-	{
+{
 	char pass[50];
 	for(int i=0;i<50;i++)
-		{
+	{
 		pass[i]=getch();
 		int intpass=int(pass[i]);
 		if(intpass==13)
-			{
+		{
 			pass[i]='\0';
 			break;
 		}
 		if(!(intpass>96&&intpass<123||intpass>64&&intpass<91||intpass>32&&intpass<43))
-			{
+		{
 			clrscr();
 			gotoxy(20,11);
 			cout<<"Use characters A-Z, a-z, ! \" # $ % & ' ( ) *";
@@ -24,4 +24,15 @@ char* getpass()
 		cout<<'*';
 	}
 	return pass;
+}
+
+void error_mssg(char mssg[])
+{
+	clrscr();
+	border('#',20,10,40,6);
+	gotoxy(27,12);
+	cout<<mssg;
+	gotoxy(26,14);
+	cout<<"Press any key to continue..";
+	getch();
 }
